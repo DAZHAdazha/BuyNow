9b62d43db016fe104d41703bf004b14cdc7bd229
DAZHA <47051206+DAZHAdazha@users.noreply.github.com>
2021-01-02 12:25:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,37 +1,37 @@
 from app import app
 import logging
 from app.models import User, Cart, CartDetail, Order, OrderDetail, Product, Wishlist, WishlistDetail
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # db.drop_all()
 
     # db.create_all()
 
-    handler = logging.FileHandler('all.log')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    app.logger.setLevel(logging.INFO)
-    handler.setFormatter(formatter)
-    app.logger.addHandler(handler)
+handler = logging.FileHandler('all.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+app.logger.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
 
-    info_handler = logging.FileHandler('info.log')
-    info_filter = logging.Filter()
-    info_filter.filter = lambda record: record.levelno < logging.WARNING
-    info_handler.setFormatter(formatter)
-    info_handler.addFilter(info_filter)
-    app.logger.addHandler(info_handler)
+info_handler = logging.FileHandler('info.log')
+info_filter = logging.Filter()
+info_filter.filter = lambda record: record.levelno < logging.WARNING
+info_handler.setFormatter(formatter)
+info_handler.addFilter(info_filter)
+app.logger.addHandler(info_handler)
 
-    warning_handler = logging.FileHandler('warning.log')
-    warning_filter = logging.Filter()
-    warning_filter.filter = lambda record:  record.levelno == logging.WARNING
-    warning_handler.setFormatter(formatter)
-    warning_handler.addFilter(warning_filter)
-    app.logger.addHandler(warning_handler)
+warning_handler = logging.FileHandler('warning.log')
+warning_filter = logging.Filter()
+warning_filter.filter = lambda record:  record.levelno == logging.WARNING
+warning_handler.setFormatter(formatter)
+warning_handler.addFilter(warning_filter)
+app.logger.addHandler(warning_handler)
 
-    error_handler = logging.FileHandler('error.log')
-    error_filter = logging.Filter()
-    error_filter.filter = lambda record: record.levelno == logging.ERROR
-    error_handler.setFormatter(formatter)
-    error_handler.addFilter(error_filter)
-    app.logger.addHandler(error_handler)
+error_handler = logging.FileHandler('error.log')
+error_filter = logging.Filter()
+error_filter.filter = lambda record: record.levelno == logging.ERROR
+error_handler.setFormatter(formatter)
+error_handler.addFilter(error_filter)
+app.logger.addHandler(error_handler)
 
     # app.logger.info("!!!")
     # app.logger.warning("Warning msg")
@@ -94,4 +94,4 @@ if __name__ == '__main__':
     # db.session.commit()
     # print(first_user.cart.cartDetails[0].product_number)
     # print(first_user.cart.cartDetails[1].product_number)
-    app.run(debug="True")
+app.run(debug="True")
